a26739fdbda0088858982815453ad4789a842140
Sujaur-Rahman <104597373+Sujaur-Rahman@users.noreply.github.com>
2023-06-17 21:04:47 -0400

    Update and rename 4_Trimmomatic_WGS2_Apr2022.sh to 2_Trimmomatic_WGS.sh

--- 4_Trimmomatic_WGS.sh
+++ 4_Trimmomatic_WGS.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /hosted/cvmpt/archive/WGS_Human/z_ROUND_2_Mar2022_TL
+cd /hosted/cvmpt/archive/WGS_Human/
 ## load the module
 
 #load the module on Easley
@@ -12,12 +12,11 @@ FILELIST=`cat FSamplesList.txt`
 
 for FILENAME in $FILELIST
 do
-#BC-CR-100-1_1_Apr2022.fastq.gz
 SHORTER=`echo $FILENAME | awk -F "." '{print $1}'`
 SHORT=`echo $SHORTER | awk -F "_" '{print $1}'`
 
 #Make sure that the path to the trimmomatic.jar file is correct
-java -jar /tools/trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 48 -phred33 -trimlog $SHORTER.trim.log "$SHORT"_1_Apr2022.fastq.gz "$SHORT"_2_Apr2022.fastq.gz -baseout $SHORT.trim.fastq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 HEADCROP:0 LEADING:0 TRAILING:0 SLIDINGWINDOW:4:10
+java -jar /tools/trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 48 -phred33 -trimlog $SHORTER.trim.log "$SHORT"_1_Apr2022.fastq.gz "$SHORT"_2_Apr2022.fastq.gz -baseout $SHORT.trim.fastq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 HEADCROP:8 LEADING:0 TRAILING:0 SLIDINGWINDOW:4:10
 
 done
 
